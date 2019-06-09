f50acb9d3ba613d3c5ab5e61ef1b7aca2b5ef8b2
Freckie <freckie@frec.kr>
2019-06-09 15:40:07 +0900

    Fix

--- api/app.py
+++ api/app.py
@@ -5,10 +5,15 @@ from flask_cors import CORS
 from flask_restful import Api
 
 from endpoints.test import Test
+
 from endpoints.users.signin import Signin
 from endpoints.users.signup import Signup
+
 from endpoints.boards.getItemInfo import GetItemInfo
 from endpoints.boards.getItemInsert import GetItemInsert
+from endpoints.boards.lostItemInfo import LostItemInfo
+from endpoints.boards.lostItemInsert import LostItemInsert
+
 from endpoints.categories.categoryGetItem import CategoryGetItem
 from endpoints.categories.categoryLostItem import CategoryLostItem
 from endpoints.categories.categoryCompleteItem import CategoryCompleteItem
@@ -30,6 +35,8 @@ api.add_resource(Signup, '/signup')
 # Endpoints - boards
 api.add_resource(GetItemInfo, '/boards/getitems')   # getitem 목록
 api.add_resource(GetItemInsert, '/boards/getitems') # getitem 추가
+api.add_resource(LostItemInfo, '/boards/lostitems')   # losttitem 목록
+api.add_resource(LostItemInsert, '/boards/lostitems') # lostitem 추가
 
 # Endpoints - categories
 api.add_resource(CategoryGetItem, '/categories/<int:c_id>/getitems')
@@ -39,8 +46,15 @@ api.add_resource(CategoryCompleteItem, '/categories/<int:c_id>/completeitems')
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         port = sys.argv[1]
+        if 'release' in sys.argv[2].strip():
+            release = True
+        else:
+            release = False
     else:
         port = 8000
+        release = False
 
-    #app.run(debug=False, host='0.0.0.0', port=port)    # for release
-    app.run(debug=False, host='127.0.0.1', port=port)   # for debug
+    if release:
+        app.run(debug=False, host='0.0.0.0', port=port)    # for release
+    else:
+        app.run(debug=True, host='127.0.0.1', port=port)   # for debug
--- api/endpoints/boards/lostItemInsert.py
+++ api/endpoints/boards/lostItemInsert.py
@@ -6,17 +6,20 @@ import database
 
 class LostItemInsert(Resource):
     def post(self):
-        req_json = request.get_json(force=True)
-        _i_id = req_json['i_id']
-        _u_id = req_json['u_id']
-        _title = req_json['title']
-        _content = req_json['content']
-        _c_id = req_json['c_id']
+        try:
+            req_json = request.get_json(force=True)
+            _u_id = req_json['u_id']
+            _title = req_json['title']
+            _content = req_json['content']
+            _c_id = req_json['c_id']
+        except KeyError:
+            return {'status': 400, 'message': 'Invalid parameter.'}, 400
 
         # db
         db = database.db_connect()
-        sql = 'insert into lostitem(i_id, u_id, title, content, c_id) values(\'{}\', \'{}\', \'{}\', \'{}\', \'{}\')'.format(_i_id, _u_id, _title, _content, _c_id)
+        sql = 'INSERT INTO lostitem(u_id, title, content, c_id) VALUES ({}, \'{}\', \'{}\', {})'.format(_u_id, _title, _content, _c_id)
         curs = db.cursor()
         curs.execute(sql)
         db.commit()
-        #rows = curs.fetchall()
+
+        return {'status': 200}
