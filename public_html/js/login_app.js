7faaeb85007f9df0120f41165249890c649d101b
earthssu <entjq9502@khu.ac.kr>
2019-06-12 02:56:01 +0900

    login_app.js

--- public_html/js/login_app.js
+++ public_html/js/login_app.js
@@ -1,15 +1,33 @@
-function test(){
-  var param = $("form[name=form1]").serialize();
+const formEl = document.querySelector(`form`);
+formEl.addEventListener(`submit`, (e) => {
+  e.preventDefault();
 
- $.ajax({   
-    type: "POST",
-    url: "/test.do",
-    data: param,
-    success:function(data){
-      alert("성공");
-    },
-    error:function(data){
-      alert("error");
-    }
-  });
-} 
+  //콘솔로 받은 값 확인
+  var user_input_id = $('input[name=id]').val();
+  var user_input_pw = $('input[name=password]').val();
+  console.log(user_input_id);
+  console.log(user_input_pw);
+
+  //axios를 이용해 post형식으로 서버에 정보 확인.
+  axios.post("/api/signin", {
+    id: $('input[name=id]').val(),
+    password: $('input[name=password]').val()
+  })
+    //성공시 logged_in.html로 redirect
+    .then(function (response) {
+      if(response.status == 200)
+      {
+        console.log(response);
+        console.log("success");
+    }      
+    })
+    //실패시 페이지 reload
+    .catch(function (error) {
+      if(error.status == 401)
+      {
+        console.log(error);
+        console.log("fail");
+      }
+      
+    })
+});
