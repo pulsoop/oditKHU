eed98f3fcf5771e0a3e7110b52323cbd31ec6a52
freckie <freckie@frec.kr>
2019-06-12 20:28:16 +0900

    플젝 망해써오

--- api/endpoints/boards/getItemArticle.py
+++ api/endpoints/boards/getItemArticle.py
@@ -10,7 +10,7 @@ class GetItemArticle(Resource):
     def get(self, i_id):
         # 게시글 데이터.
         db = database.db_connect()
-        sql = 'SELECT u.id as "author_id", u.name as "author_name", title, content, register_date, get_date FROM getitem AS gi JOIN user AS u ON gi.u_id = u.u_id WHERE gi.i_id={};'.format(i_id)
+        sql = 'SELECT u.id as "author_id", u.name as "author_name", title, content, register_date, get_date, gimag.url FROM getitem AS gi JOIN user AS u ON gi.u_id = u.u_id JOIN getimage AS gimag ON gi.i_id = gimag.i_id WHERE gi.i_id={};'.format(i_id)
         curs = db.cursor()
         curs.execute(sql)
 
@@ -28,6 +28,7 @@ class GetItemArticle(Resource):
             'content': '',
             'register_date': '',
             'get_date': '',
+            'image': '',
             'comment_count': 0,
             'comments': list()
         }
@@ -39,8 +40,13 @@ class GetItemArticle(Resource):
         result['register_date'] = row[4].strftime('%Y-%m-%d %H:%M:%S')
         result['get_date'] = result['register_date']
 
+        if row[6] is not None:
+            result['image'] = 'http://oditkhu.dasom.io/img/items/' + row[6]
+        else:
+            result['image'] = 'http://oditkhu.dasom.io/img/items/no-image-icon.jpg'
+
         # comments
-        sql = 'select u.id, u.name, allcomment.content from(select g_com.u_id, g_com.content from (getcomment as g_com join getitem as g_i on g_i.i_id = g_com.i_id) where g_i.i_id = 1) as allcomment join user as u on allcomment.u_id = u.u_id;'
+        sql = 'select u.id, u.name, allcomment.content from(select g_com.u_id, g_com.content from (getcomment as g_com join getitem as g_i on g_i.i_id = g_com.i_id) where g_i.i_id = {}) as allcomment join user as u on allcomment.u_id = u.u_id;'.format(i_id)
         curs = db.cursor()
         curs.execute(sql)
 
