4e54977b3e89faa378ac3368c39be5be8ab84466
freckie <freckie@frec.kr>
2019-06-12 20:15:19 +0900

    Fix

--- api/endpoints/boards/getItemInfo.py
+++ api/endpoints/boards/getItemInfo.py
@@ -10,7 +10,7 @@ class GetItemInfo(Resource):
     def get(self):
         # db
         db = database.db_connect()  # connection객체
-        sql = 'SELECT u.id, gi.title, gi.register_date, gi.get_date, gi.i_id, gimg.url FROM getitem AS gi JOIN user AS u ON gi.u_id = u.u_id JOIN getimage AS gimg ON gi.i_id = gimg.i_id;'
+        sql = 'SELECT u.id, gi.title, gi.register_date, gi.get_date, gi.i_id, gimg.url FROM getitem AS gi JOIN user AS u ON gi.u_id = u.u_id LEFT OUTER JOIN getimage AS gimg ON gi.i_id = gimg.i_id;'
         curs = db.cursor()
         curs.execute(sql)
 
